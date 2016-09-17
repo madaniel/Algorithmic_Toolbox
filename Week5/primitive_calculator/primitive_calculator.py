c0d1a99240cad3a5a19349fd8c25e5d312dd1f6c
Daniel Madar <m.a.daniel@gmail.com>
2016-09-17 12:40:59 +0300

    looks good and tested

--- Week5/primitive_calculator/primitive_calculator.py
+++ Week5/primitive_calculator/primitive_calculator.py
@@ -23,42 +23,69 @@ def better(number):
 
     operations = [""] * (number + 1)
 
-    for dev in max_division_list:
-        for idx, m in enumerate(max_division_list):
-            if m == 0:
-                continue
-            min_list = []
+    for idx, m in enumerate(max_division_list):
+        if m == 0:
+            continue
+        min_list = [math.inf] * 3
 
-            # Option 1 - last number + 1
-            min_list.append(max_division_list[idx-1] + 1)
-            # Option 2 - current number / 2
-            if idx % 2 == 0:
-                min_list.append(max_division_list[idx//2] + 1)
-            # Option 3 - current number / 3
-            if idx % 3 == 0:
-                min_list.append(max_division_list[idx//3] + 1)
+        # Option 1 - last number + 1
+        min_list[0] = (max_division_list[idx-1] + 1)
+        # Option 2 - current number / 2
+        if idx % 2 == 0:
+            min_list[1] = (max_division_list[idx//2] + 1)
+        # Option 3 - current number / 3
+        if idx % 3 == 0:
+            min_list[2] = (max_division_list[idx//3] + 1)
 
-            max_division_list[idx], min_index = min(min_list), min_list.index(min(min_list))
+        max_division_list[idx], min_index = min(min_list), min_list.index(min(min_list))
 
-            # Option 1 - last number + 1
-            if min_index == 0:
-                operations[idx] = str(operations[idx]) + "+1"
+        # Option 1 - last number + 1
+        if min_index == 0:
+            operations[idx] = 1
 
-            # Option 2 - current number / 2
-            elif min_index == 1:
-                operations[idx] = str(operations[idx]) + "*2"
+        # Option 2 - current number / 2
+        elif min_index == 1:
+            operations[idx] = 2
 
-            # Option 3 - current number / 3
-            elif min_index == 2:
-                operations[idx] = str(operations[idx]) + "*3"
+        # Option 3 - current number / 3
+        elif min_index == 2:
+            operations[idx] = 3
 
-    #print (operations)
-    return max_division_list
+    sequence = []
+    idx = len(operations) - 1
+
+    while idx > 1:
+        current = int(operations[idx])
+        sequence.append(current)
+
+        if current != 1:
+            idx = (idx // current)
+        else:
+            idx -= 1
+
+    start_number = 1
+    sequence.reverse()
+
+    for i, item in enumerate(sequence):
+
+        if item == 1:
+            start_number += 1
+            sequence[i] = start_number
+        elif item == 2:
+            start_number = start_number * 2
+            sequence[i] = start_number
+        elif item == 3:
+            start_number = start_number *3
+            sequence[i] = start_number
+
+    sequence.insert(0, 1)
+
+    return sequence
 
 
-print (better(15))
+
+
 """
-
 def optimal_sequence(n):
     sequence = []
     while n >= 1:
@@ -70,12 +97,11 @@ def optimal_sequence(n):
         else:
             n = n - 1
     return reversed(sequence)
-
+"""
 input = input()
 n = int(input)
-sequence = list(optimal_sequence(n))
-print(len(sequence) - 1)
+sequence = list(better(n))
+print (len(sequence)-1)
 for x in sequence:
     print(x, end=' ')
 
-"""
