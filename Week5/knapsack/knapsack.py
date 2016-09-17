adb1c738a4fdcc9054ead5ccdd99962f74a1e02b
Daniel Madar <m.a.daniel@gmail.com>
2016-09-17 19:26:10 +0300

    tested

--- Week5/knapsack/knapsack.py
+++ Week5/knapsack/knapsack.py
@@ -2,16 +2,21 @@
 
 
 def optimal_weight(W, w):
-    # write your code here
+    # Init result
+    result = [0] * W
+    last = [0] * W
+
+    # Looping over bars
+    for bar in w:
+        # Looping over the weights
+        for idx, wei in enumerate(result):
+            if idx >= bar:
+                result[idx] = max(wei, bar + (last[idx-bar]))
+        last = result[:]
 
 
 
-
-    result = 0
-    for x in w:
-        if result + x <= W:
-            result = result + x
-    return result
+    return result[-1]
 
 
 
@@ -25,3 +30,5 @@ if __name__ == '__main__':
 
     W, n, *w = list(map(int, input.split()))
     print(optimal_weight(W, w))
+
+    #print(optimal_weight(10, [1, 4, 8]))
